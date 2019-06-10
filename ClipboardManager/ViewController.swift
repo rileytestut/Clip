763000735058a9d486eb18d945e731288717eb96
Riley Testut <riley@rileytestut.com>
2019-06-10 15:12:31 -0700

    Initial Commit

---
+++ ClipboardManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClipboardManager
+//
+//  Created by Riley Testut on 6/10/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
