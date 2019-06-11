b0cfc0ac40406a7b6c5c9b7b69d6646f169c062e
Riley Testut <riley@rileytestut.com>
2019-06-11 11:25:16 -0700

    Adds Roxas dependency

--- ClipboardManager/AppDelegate.swift
+++ ClipboardManager/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Roxas
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,8 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
         // Override point for customization after application launch.
+        
+        print(RoxasVersionNumber)
+        
         return true
     }
 
