a7667b78dd4fae95c19d8025b9c970337fd6b109
Riley Testut <riley@rileytestut.com>
2019-06-13 14:51:23 -0700

    Revises Clipboard History UI

---
+++ ClipboardManager/History/ClippingTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  ClippingTableViewCell.swift
+//  ClipboardManager
+//
+//  Created by Riley Testut on 6/13/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+import UIKit
+
+@objc(ClippingTableViewCell)
+class ClippingTableViewCell: UITableViewCell
+{
+    @IBOutlet var clippingView: UIView!
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var contentLabel: UILabel!
+    @IBOutlet var contentImageView: UIImageView!
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        
+        self.clippingView.layer.cornerRadius = 10
+        self.clippingView.layer.masksToBounds = true
+        
+        self.contentImageView.layer.cornerRadius = 10
+        self.contentImageView.layer.masksToBounds = true
+    }
+}
---
+++ ClipboardManager/History/HistoryViewController.swift
@@ -0,0 +1,163 @@
+//
+//  HistoryViewController.swift
+//  ClipboardManager
+//
+//  Created by Riley Testut on 6/10/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+import ClipKit
+import Roxas
+
+class HistoryViewController: UITableViewController
+{
+    private lazy var dataSource = self.makeDataSource()
+    
+    private var prototypeCell: ClippingTableViewCell!
+    private var cachedHeights = [NSManagedObjectID: CGFloat]()
+    
+    private lazy var dateComponentsFormatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .abbreviated
+        formatter.maximumUnitCount = 1
+        formatter.allowedUnits = [.second, .minute, .hour, .day]
+        return formatter
+    }()
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        self.tableView.dataSource = self.dataSource
+        self.tableView.prefetchDataSource = self.dataSource
+        
+        self.tableView.contentInset.top = 8
+        self.tableView.estimatedRowHeight = 0
+        
+        self.prototypeCell = ClippingTableViewCell.instantiate(with: ClippingTableViewCell.nib!)
+        self.tableView.register(ClippingTableViewCell.nib, forCellReuseIdentifier: RSTCellContentGenericCellIdentifier)        
+    }
+}
+
+private extension HistoryViewController
+{
+    func makeDataSource() -> RSTFetchedResultsTableViewPrefetchingDataSource<PasteboardItem, UIImage>
+    {
+        let fetchRequest = PasteboardItem.fetchRequest() as NSFetchRequest<PasteboardItem>
+        fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \PasteboardItem.date, ascending: false)]
+        fetchRequest.returnsObjectsAsFaults = false
+        fetchRequest.relationshipKeyPathsForPrefetching = ["representations"]
+        
+        let dataSource = RSTFetchedResultsTableViewPrefetchingDataSource<PasteboardItem, UIImage>(fetchRequest: fetchRequest, managedObjectContext: DatabaseManager.shared.persistentContainer.viewContext)
+        dataSource.cellConfigurationHandler = { (cell, item, indexPath) in
+            let cell = cell as! ClippingTableViewCell
+            cell.contentLabel.isHidden = false
+            cell.contentImageView.isHidden = true
+            
+            if Date().timeIntervalSince(item.date) < 5
+            {
+                cell.dateLabel.text = NSLocalizedString("now", comment: "")
+            }
+            else
+            {
+                cell.dateLabel.text = self.dateComponentsFormatter.string(from: item.date, to: Date())
+            }
+            
+            if let _ = item.representations.first(where: { UTTypeConformsTo($0.uti as CFString, kUTTypeImage) })
+            {
+                cell.titleLabel.text = NSLocalizedString("Image", comment: "")
+                cell.contentLabel.isHidden = true
+                cell.contentImageView.isHidden = false
+                cell.contentImageView.isIndicatingActivity = true
+            }
+            else if let representation = item.representations.first(where: { UTTypeConformsTo($0.uti as CFString, kUTTypePlainText) })
+            {
+                cell.titleLabel.text = NSLocalizedString("Text", comment: "")
+                cell.contentLabel.text = representation.stringValue
+            }
+            else if let representation = item.representations.first(where: { UTTypeConformsTo($0.uti as CFString, kUTTypeURL) })
+            {
+                cell.titleLabel.text = NSLocalizedString("URL", comment: "")
+                cell.contentLabel.text = representation.urlValue?.absoluteString
+            }
+            else
+            {
+                cell.titleLabel.text = NSLocalizedString("Unknown", comment: "")
+                cell.contentLabel.isHidden = true
+            }
+        }
+        
+        dataSource.prefetchHandler = { (item, indexPath, completionHandler) in
+            guard let representation = item.representations.first(where: { UTTypeConformsTo($0.uti as CFString, kUTTypeImage) }) else { return nil }
+            
+            return RSTBlockOperation() { (operation) in
+                guard let image = representation.imageValue?.resizing(toFill: CGSize(width: 300, height: 300)) else { return completionHandler(nil, nil) }
+                completionHandler(image, nil)
+            }
+        }
+        
+        dataSource.prefetchCompletionHandler = { (cell, image, indexPath, error) in
+            DispatchQueue.main.async {
+                let cell = cell as! ClippingTableViewCell
+
+                if let image = image
+                {
+                    cell.contentImageView.image = image
+                }
+                else
+                {
+                    cell.contentImageView.image = nil
+                }
+
+                cell.contentImageView.isIndicatingActivity = false
+            }
+        }
+        
+        return dataSource
+    }
+}
+
+extension HistoryViewController
+{
+    override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool
+    {
+        return false
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        let item = self.dataSource.item(at: indexPath)
+        
+        if let height = self.cachedHeights[item.objectID]
+        {
+            return height
+        }
+        
+        let portraitScreenHeight = UIScreen.main.coordinateSpace.convert(UIScreen.main.bounds, to: UIScreen.main.fixedCoordinateSpace).height
+        let maximumHeight: CGFloat
+        
+        if let _ = item.representations.first(where: { UTTypeConformsTo($0.uti as CFString, kUTTypeImage) })
+        {
+            maximumHeight = portraitScreenHeight / 2
+        }
+        else
+        {
+            maximumHeight = portraitScreenHeight / 3
+        }
+        
+        let widthConstraint = self.prototypeCell.contentView.widthAnchor.constraint(equalToConstant: tableView.bounds.width)
+        let heightConstraint = self.prototypeCell.contentView.heightAnchor.constraint(lessThanOrEqualToConstant: maximumHeight)
+        NSLayoutConstraint.activate([widthConstraint, heightConstraint])
+        defer { NSLayoutConstraint.deactivate([widthConstraint, heightConstraint]) }
+        
+        self.dataSource.cellConfigurationHandler(self.prototypeCell, item, indexPath)
+        
+        let size = self.prototypeCell.contentView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        self.cachedHeights[item.objectID] = size.height
+        return size.height
+    }
+}
+
