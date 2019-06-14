eeb4e97cb9d1682366ec5d0574f65aad14708026
Riley Testut <riley@rileytestut.com>
2019-06-14 10:44:09 -0700

    Requests background task execution for simulator
    Audio extension hack to access pasteboard in background doesn’t work in simulator unless we’ve requested a background task.

--- ClipboardManager/AppDelegate.swift
+++ ClipboardManager/AppDelegate.swift
@@ -43,9 +43,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func applicationDidEnterBackground(_ application: UIApplication)
+    {
+        #if targetEnvironment(simulator)
+        // Audio extension hack to access pasteboard doesn't work in simulator, so for testing just start background task.
+        RSTBeginBackgroundTask("com.rileytestut.ClipboardManager.simulatorBackgroundTask")
+        #endif
     }
 
     func applicationWillEnterForeground(_ application: UIApplication)
