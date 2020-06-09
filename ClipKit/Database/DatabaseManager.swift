4d5af8a7f4dffedde9db5d286408f422cba30e32
Riley Testut <riley@rileytestut.com>
2020-06-09 16:30:36 -0700

    [ClipKit] Fails gracefully if ALTAppGroups doesn’t exist in Info.plist
    SwiftUI previews don’t have ALTAppGroups key in Info.plist, so we now handle missing ALTAppGroups gracefully rather than crash.

--- ClipKit/Database/DatabaseManager.swift
+++ ClipKit/Database/DatabaseManager.swift
@@ -37,7 +37,9 @@ private class PersistentContainer: RSTPersistentContainer
 {
     override class func defaultDirectoryURL() -> URL
     {
-        let sharedDirectoryURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: Bundle.main.appGroups[0])!
+        guard let appGroup = Bundle.main.appGroups.first else { return super.defaultDirectoryURL() }
+        
+        let sharedDirectoryURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroup)!
         
         let databaseDirectoryURL = sharedDirectoryURL.appendingPathComponent("Database")
         try? FileManager.default.createDirectory(at: databaseDirectoryURL, withIntermediateDirectories: true, attributes: nil)
