52bd8c20a3e51bb4aa621515f7d87921106973f4
Riley Testut <riley@rileytestut.com>
2020-06-09 16:13:31 -0700

    [ClipBoard] Initial version

---
+++ ClipBoard/KeyboardViewController.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardViewController.swift
+//  ClipBoard
+//
+//  Created by Riley Testut on 5/25/20.
+//  Copyright © 2020 Riley Testut. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
